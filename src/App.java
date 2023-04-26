ecccbc8c1802cab43608116c8d25f8cdd077266c
SardeiroPaulo <paulosardeirodev@gmail.com>
2023-04-26 12:44:45 -0300

    metragem da mistura

--- src/App.java
+++ src/App.java
@@ -26,6 +26,8 @@ public class App {
         //calcular quantos galões de 3,6 litros
         double quantidadegalao = metragem / (volumegalao * rendimentoemlitros);
         double valortotalgaloes = precogalao * precogalao;
+        //quantidade a pintar para mistura
+        double metragemmistura = metragem * 1.1;
         //calcular a melhor quantidade de latas e galões considerando quantidade de latas e galões cheios e 10% de folga, arredondando para cima
         //saidas
         //apresentar o resultado dos calculos
@@ -34,6 +36,8 @@ public class App {
         System.out.printf("a quantidade de lataas é: %f e o valor total em latas é: %f", quantidadelatas, valortotallatas);
         System.out.println("");
         System.out.printf("A quantidade de galões é: %f e o valor total em galões é: %f", quantidadegalao , precogalao);
+        System.out.println();
+        System.out.printf("A metragem para mistura é: %f", metragemmistura);
     }   
 
 }
